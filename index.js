9d65701287893f7fe87a3f6102cef8cb5161443c
Your Name <you@example.com>
2021-08-08 14:23:11 +0300

    + responcive mobile layout

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+// Получил доступ к кнопке по id
 const submitButton = document.getElementById('submitButton')
+// Навешиваю обработчик события на клик на кнопку
 submitButton.onclick = (e) => {
+    // Получаю значение инпута имени класса
     const className = document.getElementById('classNameInput').value
+    // Получаю значение инпута цвет
     const background = document.getElementById('stylesInput').value
+    // Вызов функции которая применяет цвет ко всем элементам класса
     applyStyleToEveryInputByClassName(className, background)
 }
 
 const applyStyleToEveryInputByClassName = (className,color) => {
-    const newClassName = '.' + className
-    const elements = [...document.querySelectorAll(newClassName)]
+    // Добавил точку к имени класса для поиска ниже
+    const newClassName = '.' + className.toLowerCase()
+    // Нашел все элементы заданного класса
+    let elementsList = document.querySelectorAll(newClassName)
+    // Деструктуризировал их в массив (знаю непонятно как это)
+    const elements = [...elementsList]
+    // Прохожусь циклом по элементам и применяю цвет к каждому
     for(let element of elements){
-        element.style.backgroundColor = color
+        // применяю цвет к каждому
+        element.style.backgroundColor = color.toLowerCase()
     }
 }
